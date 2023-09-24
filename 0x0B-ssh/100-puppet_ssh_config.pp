0d5571203f98219ac7cb6976a55cb05bf5401a74
Uchechukwu Madumere <madumereuchechukwu@gmail.com>
2023-09-24 16:58:46 -0700

    task-3

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# a script showing how to use pupet to make changes to a file
+
+
+file {  'etc/ssh/ssh_config':
+	 ensure => present,
+
+content =>"
+
+	#SSH Client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+}
